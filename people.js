3f1a3e3255633d620e5858bb899784ffe3a80c89
salamah alf <psysa21@nottingham.ac.uk>
2024-05-14 02:52:44 +0100

    fix

--- people.js
+++ people.js
@@ -1,6 +1,6 @@
 // After install the supabase-js module
 import { createClient } from
-'@supabase/supabase-js';
+'https://esm.sh/@supabase/supabase-js@2';
 // Create a single supabase client for interacting with your database
 const supabaseUrl = 'https://zkeqajgzydydzlxruujq.supabase.co'; // Replace with your project's URL
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprZXFhamd6eWR5ZHpseHJ1dWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1MzczNDgsImV4cCI6MjAzMTExMzM0OH0.M7f-1vQvuNBcBUk4sz6ldNOhXd4DzJYDfHpeFBH01tU'; // Replace with your project's anon key
