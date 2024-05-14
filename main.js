6edb6c09c71e09320d21cfc0a64b05b123035535
salamah alf <psysa21@nottingham.ac.uk>
2024-05-14 02:24:49 +0100

    fix

--- main.js
+++ main.js
@@ -2,7 +2,10 @@
 import { createClient } from
 'https://esm.sh/@supabase/supabase-js@2';
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://your-project-url.supabase.co', 'https://zkeqajgzydydzlxruujq.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprZXFhamd6eWR5ZHpseHJ1dWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1MzczNDgsImV4cCI6MjAzMTExMzM0OH0.M7f-1vQvuNBcBUk4sz6ldNOhXd4DzJYDfHpeFBH01tU')
+const supabaseUrl = 'https://zkeqajgzydydzlxruujq.supabase.co'; // Replace with your project's URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprZXFhamd6eWR5ZHpseHJ1dWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1MzczNDgsImV4cCI6MjAzMTExMzM0OH0.M7f-1vQvuNBcBUk4sz6ldNOhXd4DzJYDfHpeFBH01tU'; // Replace with your project's anon key
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 
 async function searchPeople(query) {
   query = query.trim();  // Trim whitespace
